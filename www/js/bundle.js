72c8b1a10d0ba15f32ca7566fb713f12beb81aea
Damian Lewis <damian@damianlewis.co.uk>
2014-05-06 08:20:37 +0100

    Broadcast location data in alert mode.

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -14034,14 +14034,22 @@ module.exports = _.extend({}, Backbone.Events);
 "use strict";
 
 var Backbone = require('backbone');
+var _ = require('underscore');
 var $ = require('jquery');
 var template = require("../templates/Alert.hbs");
 Backbone.$ = $;
 
 module.exports = Backbone.View.extend({
 
+    events: {
+        "click #back-button": "cancel"
+    },
+
     initialize: function(options) {
+        console.log("alert initialize");
+        _.bindAll(this, 'locationSuccess', 'locationError', 'update');
         this.render();
+        this.startTracking();
     },
 
     render: function() {
@@ -14049,9 +14057,40 @@ module.exports = Backbone.View.extend({
         return this;
     },
 
+    startTracking: function() {
+        this.update();
+    },
+
+    getLocation: function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.locationSuccess, this.locationError);
+        }
+    },
+
+    update: function() {
+        this.getLocation();
+        this.trackingId = setTimeout(this.update, 10000); // Track every 10 seconds
+    },
+
+    broadcastLocation: function(position) {
+        console.log("Device tracked to latitude " + position.coords.latitude + " longitude " + position.coords.longitude + " on " + position.timestamp);
+    },
+
+    locationSuccess: function(position) {
+        this.broadcastLocation(position);
+    },
+
+    locationError: function(error) {
+        console.log(error.messaga);
+    },
+
+    cancel: function() {
+        clearTimeout(this.trackingId);
+    }
+
 });
 
-},{"../templates/Alert.hbs":19,"backbone":6,"jquery":16}],33:[function(require,module,exports){
+},{"../templates/Alert.hbs":19,"backbone":6,"jquery":16,"underscore":17}],33:[function(require,module,exports){
 "use strict";
 
 var Backbone = require('backbone');
