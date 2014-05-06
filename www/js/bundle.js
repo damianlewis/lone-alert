2df9162ab22e28e208463697bc8a8d7239580e8d
Damian Lewis <damian@damianlewis.co.uk>
2014-05-06 08:24:10 +0100

    Added mock data.

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -46,9 +46,12 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 
 var appointments = [
-    {"id": 1, "firstName": "Damian", "lastName": "Lewis", "address": "123 Market St", "city": "Hemel Hempstead", "county": "Herts", "postCode": "HP12 4GH", "latitude": 51.7140143, "longitude": -0.3340304, "date": "2014-04-28", "time": "10:00", "duration": 3, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
-    {"id": 2, "firstName": "Freya", "lastName": "Lewis", "address": "54 High St", "city": "St Albans", "county": "Herts", "postCode": "AL1 4DF", "latitude": 37.785868, "longitude": -122.4064973, "date": "2014-04-28", "time": "11:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
-    {"id": 3, "firstName": "Thoms", "lastName": "Lewis", "address": "78 Station Rd", "city": "Hatfield", "county": "Herts", "postCode": "AL10 6GH", "latitude": 53.7140143, "longitude": -0.5340304, "date": "2014-04-28", "time": "12:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
+    {"id": 1, "firstName": "Damian", "lastName": "Lewis", "address": "59 Radlett Rd", "city": "St Albans", "county": "Herts", "postCode": "AL2 2JX", "latitude": 51.714014, "longitude": -0.334030, "date": "2014-04-28", "time": "10:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
+    {"id": 2, "firstName": "Louise", "lastName": "Khan", "address": "96 St Andrews Lane", "city": "Daglingworth", "county": "Herts", "postCode": "GL7 4NW", "latitude": 51.478395, "longitude": -1.686164, "date": "2014-04-28", "time": "11:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
+    {"id": 3, "firstName": "Rosie", "lastName": "Little", "address": "763 Warren St", "city": "West Glen", "county": "Herts", "postCode": "PA21 4GA", "latitude": 55.964751, "longitude": -5.223464, "date": "2014-04-28", "time": "12:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
+    {"id": 4, "firstName": "Amy", "lastName": "McDonald", "address": "48 Bullwood Rd", "city": "St George's", "county": "Herts", "postCode": "CF5 6DZ", "latitude": 51.519193, "longitude": -3.303795, "date": "2014-04-28", "time": "13:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
+    {"id": 5, "firstName": "Jude", "lastName": "Lambert", "address": "78 Monks Way", "city": "Tong Norton", "county": "Herts", "postCode": "TF11 9XY", "latitude": 52.382165, "longitude": -2.514461, "date": "2014-04-28", "time": "14:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
+    {"id": 6, "firstName": "Lucas", "lastName": "Rowe", "address": "83 Peachfield Road", "city": "Cefneithin", "county": "Herts", "postCode": "SA14 7XL", "latitude": 51.647175, "longitude": -3.783235, "date": "2014-04-28", "time": "15:00", "duration": 45, "startTime": 0, "actual": 0, "additional": 0, "status": "booked"},
 ];
 
 var getAll = function() {
